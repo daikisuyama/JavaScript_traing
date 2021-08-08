08c6eeeeddbdd5ba3222bde784fccabc8bc84a94
daikisuyama <utdaiki19990122@gmail.com>
2021-08-08 16:52:35 +0900

    Start Introduction

---
+++ Introduction/53.js
@@ -0,0 +1,4 @@
+let name_sample= "鈴木";
+let str=`こんにちは、${name_sample}さん。
+今日もいい天気ですね！`;
+console.log(str);
---
+++ Introduction/57.js
@@ -0,0 +1,6 @@
+var x;
+var obj={x:1,y:2};
+var func=()=>{};
+console.log(x);
+console.log(obj.z);
+console.log(func());
