d0eb3f9e59070d623faf427cccefbf5a7643fcc3
daikisuyama <utdaiki19990122@gmail.com>
2021-08-10 22:18:47 +0900

    Add 113.js

---
+++ Introduction/113.js
@@ -0,0 +1,9 @@
+// String型のインスタンスを生成
+let str1="こんばんは";
+// lengthプロパティの値を出力
+console.log(str1.length)
+
+// String型のインスタンスを"明示的に"生成
+let str2=new String("こんばんは！");
+// lengthプロパティの値を出力
+console.log(str2.length);
